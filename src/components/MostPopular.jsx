16107ecc8b513232d4450236361feb24dc6905c4
MarianoXinos1 <xinosmariano@gmail.com>
2024-06-06 14:47:33 -0300

    Add: División de la estructura del ContentWrap + Revisión final del repositirio'

---
+++ src/components/MostPopular.jsx
@@ -0,0 +1,136 @@
+
+
+
+function MostPopular() {
+    return(
+        
+        <section className="content">
+        <h2>Más populares</h2>
+        <article className="person-boxes">
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/avatar.jpg" alt="Avatar"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">Avatar: The Way of Water</h2>
+                    <p className="bio-position">Ciencia Ficción</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/john-wick.jpg" alt="john-wick"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">John Wick: Chapter 4</h2>
+                    <p className="bio-position">Acción</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/shazan.jpg" alt="shazan"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">Shazam! Fury of the Gods</h2>
+                    <p className="bio-position">Fantasía</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/sayen.jpg" alt="sayen"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">Sayen</h2>
+                    <p className="bio-position">Acción</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/the-magicians-elephant.jpg" alt="Avatar"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">The Magician Elephant</h2>
+                    <p className="bio-position">Animación</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+            <div className="person-box shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div className="box-avatar">
+                    <img src="../img/sniper.jpg" alt="Avatar"/>
+                </div>
+                <div className="box-bio">
+                    <h2 className="bio-name">Sniper: The White Raven</h2>
+                    <p className="bio-position">Guerra</p>
+                </div>
+                <div className="box-actions">
+                    <button>
+                        <i className="bi bi-star"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-chat"></i>
+                    </button>
+                    <button>
+                        <i className="bi bi-envelope"></i>
+                    </button>
+                </div>
+            </div>
+        </article>
+    </section>
+        
+    )
+}
+
+export default MostPopular;
---
+++ src/components/PreSale.jsx
@@ -0,0 +1,16 @@
+
+function PreSale() {
+    return(
+        
+        <header className="preventa">
+            <h2>Preventa</h2>
+            <div>
+                <img src="./img/banner.jpg" alt="Trolls 2"/>
+            </div>
+    
+         </header>
+        
+    )
+}
+
+export default PreSale;
