974c361bb66019dccc930b350bc498022f717746
MarianoXinos1 <xinosmariano@gmail.com>
2024-06-06 13:01:55 -0300

    Add: Creacion de componentes para el menu lateral

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,13 @@
 import SearchWrap from "./SearchWrap";
-import logoDh from "../assets/img/logo-dh.png";
+import MenuWrap  from "./MenuWrap";
+
 
 function Dashboard() {
     return(
         
         <div className= 'dashboard'> 
             <SearchWrap/>
-            <div className="user-avatar">
-					<img src={logoDh} alt="Logo Digital House	"/>
-			</div>
+            <MenuWrap/>
         </div>
         
     )
---
+++ src/components/MenuWrap.jsx
@@ -0,0 +1,66 @@
+import logoDh from "../assets/img/logo-dh.png";
+const iconStyle = {
+    "font-size": "1.2rem",
+    "color": "cornflowerblue"
+}
+
+function MenuWrap() {
+    return(
+        <header className="menu-wrap">
+        <figure className="user">
+            <div className="user-avatar">
+					<img src={logoDh} alt="Logo Digital House	"/>
+			</div>
+            <figcaption>
+                Digital House
+            </figcaption>
+        </figure>
+        <nav>
+            <section className="dicover">
+                <h3>Opciones</h3>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-film" style={iconStyle}></i>
+                            - Películas
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-play-circle" style={iconStyle}></i>
+                            - Cines
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-person" style={iconStyle}></i>
+                            - Géneros
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-bar-chart"></i>
+                            - Más populares
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-tags"></i>
+                            - Promociones
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="bi bi-graph-up"></i>
+                            - Estadísticas
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </nav>
+    </header>
+   
+    )
+}
+
+export default MenuWrap;
